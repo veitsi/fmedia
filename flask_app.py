93781ed06544fe957e7012fb2b51df053e5d7960
pydev <xtfkpi@gmail.com>
2016-05-23 23:56:12 +0300

    it fucking saves to server

--- flask_app.py
+++ flask_app.py
@@ -2,34 +2,44 @@
 import os
 from flask import Flask, render_template,\
     request, redirect, url_for, send_from_directory
+import base64
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 
 @app.route('/')
 def hello_world():
-    return render_template('uploader.template.html')
+    return render_template('index.template.html')
 
+@app.route('/upload', methods=['GET'])
+def upload_start():
+    print ("upload page")
+    return render_template("upload.template.html")
 
 # Route that will process the file upload
 @app.route('/upload', methods=['POST'])
 def upload():
+    print('we have some POST from you')
+    img = request.form['img'][22:]
+    print(img)
+    f=open('current.png','wb')
+    f.write(base64.b64decode(img))
+    f.close()
+    return "Uploaded"
+
     # Get the name of the uploaded file
-    file = request.files['file']
     # Check if the file is one of the allowed types/extensions
-    if True: #file and allowed_file(file.filename):
+    #file and allowed_file(file.filename):
         # Make the filename safe, remove unsupported chars
-        filename = file.filename #secure_filename(file.filename)
+        # filename = file.filename #secure_filename(file.filename)
         # Move the file form the temporal folder to
         # the upload folder we setup
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         # Redirect the user to the uploaded_file route, which
         # will basicaly show on the browser the uploaded file
-        return "Hello"
             # redirect(url_for('uploaded_file',
             #                     filename=filename))
 
-
 # make comment for pythonanywhere
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
