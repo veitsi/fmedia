5c775e68ee55b58026889ced5842f0261d0d2325
pydev <xtfkpi@gmail.com>
2016-05-23 15:08:25 +0300

    flask gives static files

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,14 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Shopeiro!'
+    return render_template('index.template.html')
+
+# make comment for pythonanywhere
+# if __name__ == "__main__":
+#     app.run(debug=True)
