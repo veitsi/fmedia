846f08c461aa78ae75e5b330d282ed27d6a5f5e6
pydev <xtfkpi@gmail.com>
2016-05-24 11:47:47 +0300

    saves files with random names

--- flask_app.py
+++ flask_app.py
@@ -3,10 +3,13 @@ import os
 from flask import Flask, render_template, \
     request, redirect, url_for, send_from_directory
 import base64
+import random, string
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 
+def randomword(length):
+    return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
 
 def log(msg):
     f = open('log.txt', 'a')
@@ -16,7 +19,7 @@ def log(msg):
 
 @app.route('/')
 def hello_world():
-    log('we start /')
+    log('we start /'+randomword(20))
     return render_template('index.template.html')
 
 
@@ -29,28 +32,13 @@ def upload_start():
 # Route that will process the file upload
 @app.route('/upload', methods=['POST'])
 def upload():
-    log('we have some POST from you')
     img = request.form['img'][22:]
-    print(img)
-    f = open('current.png', 'wb')
+    f = open('uploads/'+randomword(20)+'.png', 'wb')
     f.write(base64.b64decode(img))
     f.close()
     log("Uploaded")
 
-    # Get the name of the uploaded file
-    # Check if the file is one of the allowed types/extensions
-    # file and allowed_file(file.filename):
-    # Make the filename safe, remove unsupported chars
-    # filename = file.filename #secure_filename(file.filename)
-    # Move the file form the temporal folder to
-    # the upload folder we setup
-    # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    # Redirect the user to the uploaded_file route, which
-    # will basicaly show on the browser the uploaded file
-    # redirect(url_for('uploaded_file',
-    #                     filename=filename))
-
 
 # make comment for pythonanywhere
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
