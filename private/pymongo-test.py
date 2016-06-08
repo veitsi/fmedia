93b653c1cf01398706d2bbd56228e02b91a900df
pydev <xtfkpi@gmail.com>
2016-06-08 21:58:24 +0300

    meteor. files added

---
+++ private/pymongo-test.py
@@ -0,0 +1,6 @@
+# import pymongo
+# client = pymongo.MongoClient('localhost', 27017)
+# db = client['test-database']
+# collection = db['test-collection']
+# collection.insert({'name':'Canada'})
+# print(collection.find())
