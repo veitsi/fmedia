2e8fb6b9433b006f5472cf61a814735dda281c71
pydev <xtfkpi@gmail.com>
2016-06-09 14:48:12 +0300

    grouping by size and price

--- obj.js
+++ obj.js
@@ -1,5 +1,5 @@
 objects=[
-    {'_id':'1', 'name':'Canada', 'pickup':false},
-    {'_id':'2', 'name':'Suomi', 'pickup': true}
+    {'_id':'AA1', 'name':'Canada', 'pickup':false, 'size':[40,42]},
+    {'_id':'BB2', 'name':'Suomi', 'pickup': true}
 ];
 mobj = new Mongo.Collection('mobj');
--- private/xml-test.py
+++ private/xml-test.py
@@ -10,27 +10,72 @@ assert key2value_str('_id', 123456) == "'_id':'123456'"
 
 def node2json(node):
     rez = "{"
-    #print(node.tag, node.keys(), node.values())
-    rez += key2value_str('_id:', node.get('id') )
+    # print(node.tag, node.keys(), node.values())
+    rez += key2value_str('_id:', node.get('id'))
     # if len(node)!=25:
+    pictures_mode = False
     for subnode in node:
-        # print(subnode.tag)
-        if subnode.tag == 'price' or subnode.tag=='market_category' or subnode.tag=='typePrefix' or subnode.tag=='description':
-            rez =rez+','+key2value_str(subnode.tag, subnode.text)
-        print(subnode.tag, subnode.keys(), subnode.text)
-    rez = rez + "}\n"
+        # print(subnode.tag, subnode.keys(), subnode.values(), subnode.text)
+        if pictures_mode and subnode.tag != 'picture':
+            pictures_mode = False
+            rez = rez + "]"
+        if subnode.tag == 'market_category' or subnode.tag == 'typePrefix' or subnode.tag == 'description':
+            rez = rez + ',' + key2value_str(subnode.tag, subnode.text)
+        elif subnode.tag == 'picture':
+            if pictures_mode:
+                rez = rez + ",'" + subnode.text + "'"
+            else:
+                pictures_mode = True
+                rez = rez + ",pictures:['" + subnode.text + "'"
+        elif subnode.tag == "param":
+            rez = rez + "," + key2value_str(subnode.values()[0], subnode.text)
+    rez = rez + "}"
     return rez
+
+
+def issue_size(node):
+    for subnode in node:
+        if len(subnode.values()) == 2:
+            return subnode.text
+
+
+def issue_price(node):
+    for subnode in node:
+        if subnode.tag == 'price':
+            return float(subnode.text)
 
 
 # yml parser
 # tree = etree.parse('database-lamoda.xml')
 tree = etree.parse('minibase-lamoda.xml')
 nodes = tree.xpath('/yml_catalog/shop/offers/offer')
+folded_nodes = {}
+prices = {}
+sizes = {}
+print(len(nodes))
 fb = open("database.js", "w")
-fb.write("var objects={")
-for node in nodes:  # Перебираем элементы
-    print(node2json(node))
-    break
+fb.write("var objects=[\n")
+for i in range(len(nodes)):  # Перебираем элементы
+    # fb.write(node2json(nodes[i]))
+    # fb.write(",\n")
+    # print(node2json(nodes[i]))
+    current = nodes[i].get('id')[:14]
+    current_price = issue_price(nodes[i])
+    current_size = issue_size(nodes[i])
+    if current not in folded_nodes:
+        folded_nodes[current] = 1
+        prices[current] = current_price
+        sizes[current] = current_size
+    else:
+        folded_nodes[current] += 1
+        if current_price > prices[current]:
+            prices[current] = current_price
+        sizes[current] = sizes[current] + "," + current_size
+    print(nodes[i].get('id'), current, issue_price(nodes[i]), issue_size(nodes[i]))
 
-fb.write("};")
+print(len(folded_nodes))
+print(folded_nodes)
+print(prices)
+print(sizes)
+fb.write("];")
 fb.close()
